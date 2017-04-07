dde83a50955f98f1aa8824b879f2f1d6ef8f3db3
ocean <1507480980@qq.com>
2017-04-07 16:53:31 +0800

    asd

--- src/main/webapp/js/system_user_manage.js
+++ src/main/webapp/js/system_user_manage.js
@@ -29,6 +29,34 @@ function closeNewUser() {
 }
 
 
+$.ajax({
+	url:"..user/getAllUser",
+	type:"POST",
+	dataType:'json',
+	success:function back(data){
+		addData(data);
+	},
+	error:function back(data){
+		alert("系统提示：修改信息失败");
+	}
+	
+});
+
+function addData(data){
+	var r = JSON.parse(data);
+	var str;
+	var list=r.map;
+	for(var i=0;i<list.length;i++){
+		for(var j=0;j<5;j++){
+			str+="<tr><td>"+list[i][j].username+"</td><td>"+list[i][j].rolename+"</td><td>"+list[i][j].email+"</td><td>"+
+			"<button type='button' class='btn btn-xs btn-' onclick='adjust()'>编辑</button>  "+
+			"<button type='button' class='btn btn-xs btn-primary'>删除</button>"+
+			"</td></tr>";
+		}
+	}	
+	$("tbody").append(str);
+}
+
 
 
 
